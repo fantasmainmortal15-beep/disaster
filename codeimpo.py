756814690fbe1b2a5795ee4c3bb1a997ad066f3c
fantasmainmortal15-beep <fantasmainmortal15@gmail.com>
2025-11-24 16:24:43 -0300

    Update codeimpo.py

--- codeimpo.py
+++ codeimpo.py
@@ -6,13 +6,10 @@ df = pd.read_csv("global_disaster_response_2018_2024 (1).csv")
 
 with st.sidebar:
     st.write("opciones")
-    div = st.slider ("nuemero de bins(histograma):", 1,10,3)
-    st.write ("Bins seleccionados:", div)
-    st.selectbox ("paises", "tipo de desastres")
-fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+    div =  st.slider("Número de bins (histograma):", 1, 10, 2)
+    st.write("Bins seleccionados:", div)
 
 st.subheader("Histograma basado en tus datos")
-
 numeric_columns = [
         "severity_index", "casualties", "economic_loss_usd", "response_time_hours",
         "aid_amount_usd", "response_efficiency_score", "recovery_days",
@@ -20,7 +17,20 @@ numeric_columns = [
     ]
 col_hist = st.selectbox("Selecciona columna numérica", numeric_columns)
 fig, ax = plt.subplots()
-ax.hist(df[col_hist].dropna(), bins=20)
+ax.hist(df[col_hist].dropna(), bins= div)
 ax.set_title(f"Histograma de {col_hist}")
-
 st.pyplot(fig)
+
+with st.sidebar:
+    st.write("opciones")
+    dev = st.selectbox("opciones", "paises", "desastre")
+st.subheader("Gráfico de pastel (Países)")
+
+pie_data = df["country"].value_counts()
+
+fig2, ax2 = plt.subplots()
+ax2.pie(pie_data, labels=pie_data.index, autopct="%1.1f%%")
+ax2.set_title("Distribución por país")
+
+st.pyplot(fig2)
+
