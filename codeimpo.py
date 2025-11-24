ee32a0b31ddadfa703f9aafefe75ad9b9694b56f
fantasmainmortal15-beep <fantasmainmortal15@gmail.com>
2025-11-24 16:43:12 -0300

    Update codeimpo.py

--- codeimpo.py
+++ codeimpo.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 df = pd.read_csv("global_disaster_response_2018_2024 (1).csv")
 
+st.title = ("desastres naturales")
+
 with st.sidebar:
     st.write("opciones")
     div =  st.slider("Número de bins (histograma):", 1, 10, 2)
@@ -23,7 +25,7 @@ st.pyplot(fig)
 
 with st.sidebar:
     st.write("opciones")
-    dev = st.selectbox("países", "desastre")
+    dev = st.selectbox("opciones", "Tipo de desastre", "países")
 st.subheader("Gráfico de pastel (Países)")
 
 pie_data = df["country"].value_counts()
@@ -34,5 +36,12 @@ ax2.set_title("Distribución por país")
 
 st.pyplot(fig2)
 
+st.subheader("Gráfico de pastel (Tipo de desastre)")
 
+pie_data2 = df["disaster_type"].value_counts()
 
+fig3, ax3 = plt.subplots()
+ax3.pie(pie_data2, labels=pie_data2.index, autopct="%1.1f%%")
+ax3.set_title("Distribución por tipo de desastre")
+
+st.pyplot(fig3)
